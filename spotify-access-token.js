56b84ed7e7d05eff5ac3136ce929cf9cf41e1a3c
Matt Pierson <matt.pierson789@gmail.com>
2023-05-09 23:49:53 -0400

    Create Soundtrack button styling

--- spotify-access-token.js
+++ spotify-access-token.js
@@ -1,305 +1,6 @@
 
 import { getSoundtrack } from './openai-script.js';
 
-// const form = document.getElementById('soundtrack-form');
-// const container = document.getElementById('soundtrack-container');
-// const accessToken = 'BQAxHUIPe469_Un7zn_elyM1WqRL1AhwoclS0UhsCnwvTwQiEyUuYBWndiCLPpwKHnaYw71zJk0475gVghsioXThIxsM2bwaZ2Sfp5wnXYxamBx3F4DM';
-// const headers = {
-//   'Authorization': `Bearer ${accessToken}`,
-//   'Content-Type': 'application/x-www-form-urlencoded'
-// };
-
-// const displayedAlbums = [];
-// const selectedTracks = [];
-// const trackData = [];
-
-
-// form.addEventListener('submit', event => {
-//   event.preventDefault();
-//   console.log('Form submitted!');
-//   container.style.display = 'flex';
-
-// const description = document.getElementById('description').value;
-// const prompt = `Create me a 20 song soundtrack inspired by ${description}`;
-// const encodedPrompt = encodeURIComponent(prompt); // encode the prompt variable
-// const apiUrl = `http://localhost:5001/?prompt=${encodedPrompt}`;
-// console.log(prompt);
-// console.log(description);
-// console.log(apiUrl);
-
-// fetch(apiUrl)
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log('OpenAI response:', data);
-//     if (data.choices && data.choices.length > 0) {
-//       const choices = data.choices;
-
-//       // Use the parser to process the choices.text
-//       let rawSongList = choices[0].text
-//         console.log('Raw song list:', rawSongList);
-      
-//         function parseSongList(rawSongList) {
-//             const lines = rawSongList.trim().split('\n');
-//             const songList = lines.map(line => {
-//               const match1 = line.match(/^\d+\.\s*"(.+)"\s+-\s+(.+)$/); // matches "song" - "artist"
-//               const match2 = line.match(/^\d+\.\s*(?:"(.+)"|(?:"[^"]*"))\s+-\s+(.+)$/); // matches "song" by "artist"
-//               const match3 = line.match(/^\d+\.\s*(.+)\sby\s(.+)$/); // matches "song" by "artist"
-//               const match4 = line.match(/^\d+\.\s*(.+)\s-\s(.+)$/); // matches "song" - "artist"
-          
-//               if (match1) {
-//                 return {
-//                   song: match1[1],
-//                   artist: match1[2],
-//                 };
-//               } else if (match2) {
-//                 return {
-//                   song: match2[1],
-//                   artist: match2[2],
-//                 };
-//               } else if (match3) {
-//                 return {
-//                   song: match3[1],
-//                   artist: match3[2],
-//                 };
-//               } else if (match4) {
-//                 return {
-//                   song: match4[1],
-//                   artist: match4[2],
-//                 };
-//               }
-//             }).filter(Boolean);
-//             return songList;
-//           }
-                    
-//           const parsedSongList = parseSongList(rawSongList);
-//           console.log('Parsed Song List:', parsedSongList);
-
-//       let playList = [];
-
-//       for (let i = 0; i < parsedSongList.length; i++) {
-//         playList.push({ song: parsedSongList[i].song, artist: parsedSongList[i].artist});
-//       }
-
-//       console.log('Playlist:', playList);
-
-//         // Map each song title and artist name to a search query for the Spotify API
-
-//         const spotifyApiRequests = parsedSongList.map(song => {
-//             const encodedSong = encodeURIComponent(song.song);
-//             const encodedArtist = encodeURIComponent(song.artist);
-//             // const spotifyApiUrl = `https://api.spotify.com/v1/search?q=${encodedArtist}+artist:${encodedSong}&type=track&limit=1`;
-//             const spotifyApiUrl = `https://api.spotify.com/v1/search?q=track:%22${encodedSong}%22%20artist:%22${encodedArtist}%22&type=track&limit=1`;
-
-
-//             console.log('Spotify API URL:', spotifyApiUrl);
-//             return fetch(spotifyApiUrl, { headers });
-//           });
-          
-          
-//       Promise.all(spotifyApiRequests)
-//         .then(responses => Promise.all(responses.map(response => response.json())))
-//         .then(dataArray => {
-//           let html = '';
-//           dataArray.forEach(data => {
-//             if (data.tracks.items.length > 0) {
-//               const track = data.tracks.items[0];
-//               const album = track.album.name;
-//               if (!displayedAlbums.includes(album)) {
-//                 displayedAlbums.push(album);
-//                 const imageUrl = track.album.images[0].url;
-//                 const previewUrl = track.preview_url;
-//                 const tileData = { album, track, imageUrl, previewUrl };
-//                 trackData.push(tileData);
-
-//                 // Create HTML to display metadata and preview audio
-//                 html += `
-//                   <div class="tile" draggable="true" data-index="${dataArray.indexOf(data)}">
-//                     <img src="${imageUrl}" alt="${album} cover">
-//                     <p class="caption">${track.name}</p>
-//                     <audio controls style="display:none;">
-//                       <source src="${previewUrl}" type="audio/mpeg">
-//                     </audio>
-//                   </div>
-//                 `;
-//                 console.log(`Got soundtrack: ${track.name}`);
-//               }
-//             }
-//           });
-
-//           container.innerHTML = html;
-
-//         })
-
-//     }
-
-//   });
-
-//   const tiles = container.querySelectorAll('.tile');
-
-//     tiles.forEach((tile, index) => {
-//         let selected = false;
-//         let isLongClick = false;
-//         const audio = tile.querySelector('audio');
-//         const track = trackData[index].tracks.items[0];
-
-//         const LONG_CLICK_DELAY = 250; // in ms
-//         let longClickTimeout;
-
-//         function handleLongClick() {
-//             isLongClick = true;
-//             if (selected) {
-//                 // Deselect the tile
-//                 selected = false;
-//                 tile.classList.remove('selected');
-//                 const index = selectedTracks.indexOf(track);
-//                 if (index > -1) {
-//                     selectedTracks.splice(index, 1);
-//                 }
-//             } else {
-//                 // Select the tile
-//                 selected = true;
-//                 tile.classList.add('selected');
-//                 selectedTracks.push(track);
-//             }
-//         }
-
-//         tile.addEventListener('mousedown', () => {
-//             longClickTimeout = setTimeout(handleLongClick, LONG_CLICK_DELAY);
-//         });
-
-//         tile.addEventListener('mouseup', () => {
-//             if (!isLongClick) {
-//                 if (audio.paused) {
-//                     audio.play();
-//                 } else {
-//                     audio.pause();
-//                 }
-//             }
-//             clearTimeout(longClickTimeout);
-//             isLongClick = false;
-//         });
-
-//         audio.addEventListener('ended', () => {
-//             selected = false;
-//             tile.classList.remove('selected');
-//             const index = selectedTracks.indexOf(track);
-//             if (index > -1) {
-//                 selectedTracks.splice(index, 1);
-//             }
-//         });
-//     });
-// });
-
-
-        
-    
-    
-          
-
-
-
-// // attempt #2
-
-// import { getSoundtrack } from './openai-script.js';
-
-// const form = document.getElementById('soundtrack-form');
-// const container = document.getElementById('soundtrack-container');
-// const accessToken = 'BQDap7JYo-naUxPk6RTb1seTwbnCRVjQGzcKV6MYbvR8wj7YNKk2T6QbJKtCp9cjF-XoN2PrnvBq13yTWmdkbg1ZQ4UgzY6iHBCo7AulSfmsFi3ap8JJ';
-// const headers = {
-//   'Authorization': `Bearer ${accessToken}`,
-//   'Content-Type': 'application/x-www-form-urlencoded'
-// };
-
-// const description = document.getElementById('description').value;
-// const prompt = `Create me a soundtrack ${description}`;
-// const openAiApiUrl = `http://localhost:5001/?prompt=${prompt}`;
-
-// fetch(openAiApiUrl)
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log('OpenAI response:', data);
-//     const choices = data.choices;
-//     const spotifyApiRequests = [];
-
-//     // Use the parser to process the choices.text
-//     let rawSongList = choices[0].text;
-//     let no_new_lines = rawSongList.split(/\n+\n/).join('- ');
-//     let arr = no_new_lines.split(/- /);
-//     let filteredArr = arr.filter(ele => ele !== "" ? ele : undefined);
-
-//     let artists = [];
-//     let songs = [];
-
-//     filteredArr.forEach((ele, i) => {
-//         if (i % 2 === 0) {
-//             artists.push(ele);
-//         } else {
-//             songs.push(ele);
-//         }
-//     });
-
-//     let playList = [];
-
-//     for (let i = 0; i < artists.length; i++) {
-//         playList.push({ artist: artists[i], song: songs[i] });
-//     }
-
-//     // Create a Spotify API request for each song
-//     for (let i = 0; i < playList.length; i++) {
-//         const song = `${playList[i].artist} ${playList[i].song}`;
-//         const spotifyApiUrl = `https://api.spotify.com/v1/search?q=${encodeURIComponent(song)}&type=track&limit=1`;
-//         spotifyApiRequests.push(fetch(spotifyApiUrl, { headers }));
-//     }
-
-//     // Fetch metadata for each song from the Spotify API
-//     Promise.all(spotifyApiRequests)
-//       .then(responses => Promise.all(responses.map(response => response.json())))
-//       .then(trackDataArray => {
-//         const tiles = [];
-
-//         // Create a tile for each track with its metadata and audio preview
-//         for (let i = 0; i < trackDataArray.length; i++) {
-//           const trackData = trackDataArray[i];
-//           if (trackData.tracks.items.length > 0) {
-//             const track = trackData.tracks.items[0];
-//             const album = track.album.name;
-//             const imageUrl = track.album.images[0].url;
-//             const previewUrl = track.preview_url;
-
-//             const tile = `
-//               <div class="tile" draggable="true">
-//                 <img src="${imageUrl}" alt="${album} cover">
-//                 <p class="caption">${track.name}</p>
-//                 <audio controls style="display:none;">
-//                   <source src="${previewUrl}" type="audio/mpeg">
-//                 </audio>
-//               </div>
-//             `;
-//             console.log(`Got soundtrack: ${track.name}`);
-//             tiles.push(tile);
-//           }
-//         }
-
-//         if (tiles.length > 0) {
-//           container.innerHTML = tiles.join('');
-//           const audioElements = container.querySelectorAll('audio');
-//           audioElements.forEach(audio => {
-//             audio.addEventListener('play', () => {
-//               audioElements.forEach(otherAudio => {
-//                 if (otherAudio !== audio) {
-//                   otherAudio.pause();
-//                 }
-//               });
-//             });
-//           });
-//         } else {
-//           container.innerHTML = '<p>No soundtrack found</p>';
-//         }
-//       });
-// });
-
-
-
 
 // attempt #3
 
@@ -431,9 +132,8 @@ const spotifyApiRequests = parsedSongList.map(song => {
       });
 
   
-      container.innerHTML = html;
+        container.innerHTML = html;
 
-    
   
       // Add event listeners to each tile
       const tiles = container.querySelectorAll('.tile');
@@ -501,35 +201,6 @@ const spotifyApiRequests = parsedSongList.map(song => {
 });
 
   
-  
-          
-
-       
-
-
-            
-
-
-       // Assign the track data to the global variable
-
-        //   Add event listeners to tiles for playing the audio and selecting or deselecting.
-
-        //  Add event listeners to the tiles to play the audio when clicked
-        //   const tiles = document.querySelectorAll('.tile');
-        //   tiles.forEach(tile => {
-        //     const audio = tile.querySelector('audio');
-        //     tile.addEventListener('click', () => {
-        //       if (audio.paused) {
-        //         audio.play();
-        //       } else {
-        //         audio.pause();
-        //       }
-        //     });
-     
-    //   });
-
-
-// export { headers, getSoundtrack, trackDataArray };
 
 
 // attempt # 4
@@ -802,3 +473,24 @@ const spotifyApiRequests = parsedSongList.map(song => {
         //     });
         //   });
 
+
+           // Assign the track data to the global variable
+
+        //   Add event listeners to tiles for playing the audio and selecting or deselecting.
+
+        //  Add event listeners to the tiles to play the audio when clicked
+        //   const tiles = document.querySelectorAll('.tile');
+        //   tiles.forEach(tile => {
+        //     const audio = tile.querySelector('audio');
+        //     tile.addEventListener('click', () => {
+        //       if (audio.paused) {
+        //         audio.play();
+        //       } else {
+        //         audio.pause();
+        //       }
+        //     });
+     
+    //   });
+
+
+// export { headers, getSoundtrack, trackDataArray };
